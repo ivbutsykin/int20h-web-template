9a47f691abd147b97b6a3ed949c751fe3caa6513
ivbutsykin <ivbutsykin@gmail.com>
2024-01-23 22:49:04 +0200

    feat: add MUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,19 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { Box, Stack, Typography } from "@mui/material";
+import int20hLogo from "/int20h-logo.png";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Stack alignItems="center" spacing={2}>
+      <Box
+        alt="INT20H logo"
+        component="img"
+        src={int20hLogo}
+        sx={{ width: 280 }}
+      />
+      <Typography textAlign="center" variant="h1">
+        INT20H
+      </Typography>
+    </Stack>
   );
 }
 
